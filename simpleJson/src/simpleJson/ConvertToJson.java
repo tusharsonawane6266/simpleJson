6cd003e8596de586361f15d8e122d78b78d77606
tushar <tushar@tushar>
2018-06-08 14:02:17 +0530

    student class added and small Gson code in main class

--- simpleJson/src/simpleJson/ConvertToJson.java
+++ simpleJson/src/simpleJson/ConvertToJson.java
@@ -1,8 +1,24 @@
 package simpleJson;
 
+import com.google.gson.Gson;
+
 public class ConvertToJson {
 
 	public static void main(String[] args) {
-		System.out.println("hiiiii");
+		Student stud = createStudent();
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(stud);
+		System.out.println(json);
+	}
+
+	private static Student createStudent() {
+		Student stud = new Student();
+		stud.setName("tushar");
+		stud.setAddress("pune");
+		stud.setAge(21);
+		stud.setSchool("All Saints");
+		stud.setStandard("12th");
+		return stud;
 	}
 }
---
+++ simpleJson/src/simpleJson/Student.java
@@ -0,0 +1,43 @@
+package simpleJson;
+
+public class Student {
+
+	private String name;
+	private String address;
+	private int age;
+	private String school;
+	private String standard;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public String getStandard() {
+		return standard;
+	}
+	public void setStandard(String standard) {
+		this.standard = standard;
+	}
+	
+	
+}
